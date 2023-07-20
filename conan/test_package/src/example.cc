e7765a31f19b4fd7cc95766cc08a95cd7f32de2d
Alexey Timin <atimin@gmail.com>
2023-07-20 14:20:10 +0200

    fix example

--- conan/test_package/src/example.cc
+++ conan/test_package/src/example.cc
@@ -3,16 +3,12 @@
 #include <iostream>
 
 using drift_bytes::Bytes;
-using drift_bytes::Variable;
 
 int main() {
-  uint32_t val = 42;
-
+  uint8_t val{42};
   auto bytes = Bytes();
-  bytes << Variable(val);
+  bytes.scalar(val);
+  auto new_val = bytes.scalar<uint8_t>();
 
-  Variable<uint32_t> res;
-  bytes >> res;
-
-  std::cout << res.data()[0] << std::endl;
+  std::cout << new_val << std::endl;
 }
